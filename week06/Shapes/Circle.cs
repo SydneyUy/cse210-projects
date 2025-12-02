1ac9881d0090939ca9fee0e67889fa525aff1d16
SydneyUy <167969403+SydneyUy@users.noreply.github.com>
2025-12-02 21:39:08 +0400

    Added Circle,rectangle,shape and sqare, editted program

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,14 @@
+class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return 3.14 * _radius * 2;
+    }
+}
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("blue", 12);
+        Rectangle rectangle = new Rectangle("red", 14,17);
+        Circle circle = new Circle("green",4);
+
+        shapes.Add(square);
+        shapes.Add(circle);
+        shapes.Add(rectangle);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
+
     }
 }
